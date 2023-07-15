8e355d654e2555ab101a4a34391968bc1dbedc17
jhileman07 <johnhileman2026@u.northwestern.edu>
2023-07-15 02:59:47 -0500

    intro to cpp

---
+++ brocode/cpp/intro.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    
+    int x; // declaration
+    x = 5;
+    int y = 6;
+    int sum = x+y;
+
+    std::cout << x;
+    std::cout << sum << std::endl << y;
+
+    return 0;
+}
